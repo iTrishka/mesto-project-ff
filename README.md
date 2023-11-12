9df7626e7637c32a0a7fdadfad8081088e4b80c9
Kristina Khramova <snowflak@yandex.ru>
2023-11-12 19:10:15 +0300

    create functions createCard, deleteCard, addCards

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+https://github.com/iTrishka/mesto-project-ff.git
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+// DOM узлы
+const cardsListElement = document.querySelector('.places__list');
+const addCardsButtom = document.querySelector('.profile__add-button');
 
-// @todo: DOM узлы
+// Функция создания карточки
+const createCard = (cardData, deleteCard) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция создания карточки
+  newCard.querySelector('.card__title').textContent = cardData.title;
+  newCard.querySelector('.card__image').src = cardData.link;
+  newCard.querySelector('.card__image').alt = cardData.title;
 
-// @todo: Функция удаления карточки
+  const deleteCardButtom = newCard.querySelector('.card__delete-button');
+  deleteCardButtom.addEventListener( 'click', e => deleteCard(e));
 
-// @todo: Вывести карточки на страницу
+  return newCard;
+};
+
+// Функция удаления карточки
+const deleteCard = e => {
+  const cardForDelete = e.target.closest('.card');
+  cardForDelete.remove();
+};
+
+// Вывести карточки на страницу
+const addCards = cardsDataList => {
+  cardsDataList.forEach(cardData => {
+    cardsListElement.append(createCard(cardData, deleteCard));
+  });
+};
+
+addCards(initialCards);
